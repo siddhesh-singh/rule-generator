6a63842adfe6fff49507b2b3b083dfe099d47e3e
siddhesh-singh <siddhesh-singh@users.noreply.github.com>
2016-07-20 16:44:22 -0500

    add wrapped even rule function

--- src/rulegenerator.py
+++ src/rulegenerator.py
@@ -302,15 +302,107 @@ def odd_rules_0(generations, initial_config, rule_num):
      
     return auto_lattice
 
+def odd_rules(generations, initial_config, rule_num):
+    rule_bin = bin(rule_num)[2:]
+    RULE_BIT_LENGTH = 8
+    
+    if len(rule_bin) < RULE_BIT_LENGTH :
+        add_bits = RULE_BIT_LENGTH - len(rule_bin)
+        add_bit_string = '0'*add_bits
+        rule_bin = add_bit_string + rule_bin
+        
+    if rule_bin[0] == '0':
+        return odd_rules_0(generations, initial_config, rule_num)
+    else:
+        return odd_rules_1(generations, initial_config, rule_num)
 
+
+
+def even_rules_wrap(generations, initial_config, rule_num, wrap_width):
+
+    rule_bin = bin(rule_num)[2:]
+    RULE_BIT_LENGTH = 8
+    
+    if len(rule_bin) < RULE_BIT_LENGTH :
+        add_bits = RULE_BIT_LENGTH - len(rule_bin)
+        add_bit_string = '0'*add_bits
+        rule_bin = add_bit_string + rule_bin
+    
+    rule_little_endian = [int(x) for x in rule_bin[::-1]]   
+    
+    initial_config_arr = deque([int(initial_config[x]) for x in range(len(initial_config)) ])
+    
+    next_config = initial_config_arr
+    
+    if len(next_config) == 0:
+        next_config.append(0)
+    
+    if not ( len(next_config) > 1 ):
+        next_config.append(0)
+        next_config.appendleft(0)
+    
+    auto_lattice = list()
+    max_length = wrap_width
+    
+    
+    
+    for i in range(generations):
+
+        if len(next_config) < wrap_width:
+                
+            if next_config[0] != 0 or next_config[1] != 0:
+                next_config.extendleft([0,0])
+                next_config.extend([0,0])
+            if next_config[-1] != 0 or next_config[-2] != 0:
+                next_config.extend([0,0])
+                next_config.extendleft([0,0])
+                
+        while(len(next_config) > wrap_width):
+            next_config.pop()
+            next_config.popleft()
+        
+
+        auto_lattice.append(next_config)
+            
+        if i == generations - 1:
+            break
+            
+        next_config = deque(list(next_config))
+        cell_number = len(next_config)
+        
+        if cell_number < wrap_width:    
+            for j in range(1, cell_number - 1):           
+                left_cell = auto_lattice[i][j-1]
+                right_cell = auto_lattice[i][j+1]
+                current_cell = auto_lattice[i][j]
+                next_config[j] = rule_little_endian[right_cell + 2*current_cell + 4*left_cell]
+        else:
+            for j in range(0, cell_number):           
+                left_cell = auto_lattice[i][j-1]
+                right_cell = auto_lattice[i][(j+1) % cell_number]
+                current_cell = auto_lattice[i][j]
+                next_config[j] = rule_little_endian[right_cell + 2*current_cell + 4*left_cell]           
+        
+        
+    for i in range(len(auto_lattice)):
+        current_length = len(auto_lattice[i])
+        if current_length < max_length:
+            length_difference = max_length - current_length
+            extender_list = [0] * int(length_difference / 2)
+            auto_lattice[i].extend(extender_list)
+            auto_lattice[i].extendleft(extender_list)
+
+    return auto_lattice
 
 
 gens = 1000
-al = odd_rules_0(gens, '010', 101)
+al = even_rules_wrap(gens, '010', 124, 1001)
+#al = even_rules(gens, '010', 124)
 alwidth = len(al[-1])
 al = [255 if x == 0 else 0 for y in al for x in y ]
     
 auto_image = Image.new('1', (alwidth,gens))
 auto_image.putdata(al)
 auto_image.convert('RGB')
-auto_image.save('../test/testimg18.png', 'PNG')
+auto_image.save('../test/testimg21.png', 'PNG')
+
